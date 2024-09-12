4894094dfb337680409a1c13df1e5bffbbddacee
myudro33 <qanashvilinika7@gmail.com>
2024-09-12 20:13:17 +0400

    feat: create dynamic button component

---
+++ src/components/TheButton.vue
@@ -0,0 +1,10 @@
+<script setup>
+const props = defineProps(['background', 'title'])
+</script>
+
+<template>
+    <button
+        :class="[props.background ? 'text-white bg-button-background hover:bg-button-background-hover' : 'bg-transparent hover:bg-button-background border border-button-background text-button-background hover:text-white']"
+        class="px-4 py-[10px] h-12 text-base font-medium rounded-[10px]">{{ props.title
+        }}</button>
+</template>
