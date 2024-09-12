24f6593428e4c3c5b440e763cc1e26c3ff89b84d
myudro33 <qanashvilinika7@gmail.com>
2024-09-12 22:55:28 +0400

    style: add custom input border color

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,7 @@ export default {
     extend: {
       colors: {
         'border-color': '#DBDBDB',
+        'input-border': '#808A93',
         'button-background': '#F93B1D',
         'button-background-hover': '#DF3014',
         'active-filter': '#F3F3F3'
