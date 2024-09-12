46c058beb19e0164668ac281948b58fd72a7527d
myudro33 <qanashvilinika7@gmail.com>
2024-09-12 19:20:02 +0400

    feat: add header component

---
+++ src/components/TheHeader.vue
@@ -0,0 +1,12 @@
+<script setup>
+
+</script>
+
+
+
+
+<template>
+    <div class="w-full h-24 border-b border-border-color px-40 py-10">
+        <img src="../assets/LOGO-02 3.png" alt="redberry-logo">
+    </div>
+</template>
