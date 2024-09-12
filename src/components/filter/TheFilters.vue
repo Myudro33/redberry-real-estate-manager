fa4feff2fd358eca47abaac11498b026fb27d924
myudro33 <qanashvilinika7@gmail.com>
2024-09-12 23:04:45 +0400

    style: add filter list

---
+++ src/components/filter/TheFilters.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+import DeleteIcon from '../icons/DeleteIcon.vue';
+</script>
+
+<template>
+    <div class="mt-4 flex items-center bg-green-500">
+        <span class="flex items-center rounded-3xl border border-border-color px-2 py-1">თბილისი
+            <DeleteIcon class="ml-1" />
+        </span>
+        <p class="font-bold ml-4">გასუფთავება</p>
+    </div>
+</template>
