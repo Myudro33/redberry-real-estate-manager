e6577d2faf1a6b97ccf272fb768c8b09f8c1d904
myudro33 <qanashvilinika7@gmail.com>
2024-09-12 21:11:10 +0400

    style: add custom box-shadow

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,6 +7,9 @@ export default {
         'border-color': '#DBDBDB',
         'button-background': '#F93B1D',
         'button-background-hover': '#DF3014'
+      },
+      boxShadow: {
+        'box-shadow': '5px 5px 12px 0px rgba(2, 21, 38, 0.08)'
       }
     }
   },
