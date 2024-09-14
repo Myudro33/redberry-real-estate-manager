6e9cfc02e3cf827d2b06b70b2fa1b292d1439dbf
myudro33 <qanashvilinika7@gmail.com>
2024-09-14 11:35:39 +0400

    feat: add custom input component

---
+++ src/components/TheInput.vue
@@ -0,0 +1,15 @@
+<template>
+    <div class="flex h-20  flex-col">
+        <label class="font-semibold text-sm" :for="props.name">{{ props.label }}</label>
+        <Field validate-on-change :name="props.name" :rules="props.rules" v-slot="{ field }">
+            <input class="h-11 p-2 shrink-0 rounded-lg border border-border-color" v-bind="field" type="text"
+                :name="props.name" />
+            <ErrorMessage class="text-error" :name="props.name" />
+        </Field>
+    </div>
+</template>
+
+<script setup>
+import { ErrorMessage, Field } from 'vee-validate';
+const props = defineProps(['name', 'rules', 'label'])
+</script>
