98045898e67c0f959398940c36478ccfb1ae0457
myudro33 <qanashvilinika7@gmail.com>
2024-09-14 11:37:03 +0400

    fix: assign type submit to button component if it isn t link

--- src/components/TheButton.vue
+++ src/components/TheButton.vue
@@ -7,7 +7,7 @@ const props = defineProps(['background', 'title', 'type', 'routerTo'])
         :class="[props.background ? 'text-white bg-button-background hover:bg-button-background-hover' : 'bg-transparent hover:bg-button-background border border-button-background text-button-background hover:text-white']"
         class="px-4 py-[10px] h-12 text-base font-medium rounded-[10px]">{{ props.title
         }}</router-link>
-    <button v-else
+    <button v-else type="submit"
         :class="[props.background ? 'text-white bg-button-background hover:bg-button-background-hover' : 'bg-transparent hover:bg-button-background border border-button-background text-button-background hover:text-white']"
         class="px-4 py-[10px] h-12 text-base font-medium rounded-[10px]">{{ props.title }}
     </button>
