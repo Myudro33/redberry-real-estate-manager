d1ff2dd4d92433f59e83e074519d322e6f36d04f
myudro33 <qanashvilinika7@gmail.com>
2024-09-14 20:03:45 +0400

    feat: loop over listings and display listing cards

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,21 @@
 <script setup>
+import ListingCard from '@/components/ListingCard.vue';
 import TheNavbar from '@/components/TheNavbar.vue';
+import { useEstateStore } from '@/stores';
+import { onMounted } from 'vue';
+const estateStore = useEstateStore()
+onMounted(() => {
+  estateStore.getListing()
+})
 </script>
 
 <template>
-  <div class="w-full px-40">
+  <div class="w-full px-40 pb-20">
     <TheNavbar />
+    <div class="flex justify-between flex-wrap gap-3 mt-8">
+      <ListingCard v-for="items in estateStore.listings" :key="items.id" :area="items.area" :bedrooms="items.bedrooms"
+        :deal="items.is_rental" :id="items.id" :image="items.image" :location="items.address"
+        :postal-code="items.zip_code" :price="items.price" />
+    </div>
   </div>
 </template>
