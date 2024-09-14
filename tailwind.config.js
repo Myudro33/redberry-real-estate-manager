89d82b6a889a16ddc01f84ae9b8a7fd2bcaf82ee
myudro33 <qanashvilinika7@gmail.com>
2024-09-14 20:02:31 +0400

    chore: add custom styles

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,7 +10,9 @@ export default {
         'button-background-hover': '#DF3014',
         'active-filter': '#F3F3F3',
         error: '#F93B1D',
-        success: '#45A849'
+        success: '#45A849',
+        'deal-type': '#02152680',
+        'gray-text': '#021526B2'
       },
       boxShadow: {
         'box-shadow': '5px 5px 12px 0px rgba(2, 21, 38, 0.08)'
