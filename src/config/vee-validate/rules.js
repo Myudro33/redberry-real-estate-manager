48535b092e0691f92043d534361551140b60ca2d
myudro33 <qanashvilinika7@gmail.com>
2024-09-15 20:04:55 +0400

    fix: change error texts

--- src/config/vee-validate/rules.js
+++ src/config/vee-validate/rules.js
@@ -1,31 +1,79 @@
+export const agentSchema = {
+  name: (value) => {
+    if (!value) {
+      return 'სახელი სავალდებულოა'
+    } else if (value.length < 3) {
+      return 'უნდა შედგებოდეს 2-ზე მეტი სიმბოლოსგან'
+    }
+    return true
+  },
+
+  surname: (value) => {
+    if (!value) {
+      return 'გვარი სავალდებულოა'
+    } else if (value.length < 3) {
+      return 'უნდა შედგებოდეს 2-ზე მეტი სიმბოლოსგან'
+    }
+    return true
+  },
+  email: (value) => {
+    if (!value) {
+      return 'ელ-ფოსტა სავალდებულოა'
+    } else if (!value.endsWith('@redberry.ge')) {
+      return 'მეილი უნდა მთავრდებოდეს @redberry.ge-თ '
+    }
+    return true
+  },
+  number: (value) => {
+    const phoneRegex = /^5\d{8}$/
+    if (!value) {
+      return 'ნომერი სავალდებულოა'
+    } else if (isNaN(value) || value.trim() === '') {
+      return 'ნომერი უნდა შედგებოდეს რიცხვებისგან'
+    } else if (!phoneRegex.test(value)) {
+      return 'უნდა იყოს შემდეგი ფორმატით 5XXXXXXXX'
+    }
+    return true
+  },
+  file: (value) => {
+    if (!value) return 'აირჩიეთ ფაილი'
+    else if (!value.startsWith('blob')) {
+      const fileSizeMB = value[0].size / (1024 * 1024)
+      return fileSizeMB <= 1 ? true : `სურათი უნდა იყოს 1მბ-ზე ნაკლები`
+    }
+    return true
+  }
+}
+
 export const schema = {
   deal: (value) => {
     if (value) {
       return true
     }
-    return 'You must choose a deal'
+    return 'აირჩიეთ განცხადების ტიპი'
   },
   file: (value) => {
-    if (!value) return 'No file selected'
+    if (!value) return 'აირჩიეთ ფაილი'
+    else if (!value.startsWith('blob')) {
+      const fileSizeMB = value[0].size / (1024 * 1024)
+      return fileSizeMB <= 1 ? true : `სურათი უნდა იყოს 1მბ-ზე ნაკლები`
+    }
     return true
-    // const fileSizeMB = value[0].size / (1024 * 1024)
-    // return fileSizeMB <= 1 ? true : `File must be less than or equal to 1 MB`
   },
   address: (value) => {
-    if (value) {
-      if (value.length < 3) {
-        return 'address must be more than 2 symbols'
-      }
-      return true
+    if (!value) {
+      return 'მისამართი სავალდებულოა'
+    } else if (value.length < 3) {
+      return 'უნდა შედგებოდეს 2-ზე მეტი სიმბოლოსგან'
     }
-    return 'you must choose a file'
+    return true
   },
   postalCode: (value) => {
     if (value === undefined || value === null || value.trim() === '') {
-      return 'This field is required'
+      return 'ფოსტის კოდი სავალდებულოა'
     }
     if (isNaN(value) || value.trim() === '') {
-      return 'Value must be numeric'
+      return 'უნდა შედგებოდეს რიცხვებისგან'
     }
     return true
   },
@@ -33,45 +81,49 @@ export const schema = {
     if (value) {
       return true
     }
-    return 'you must choose region'
+    return 'რეგიონი სავალდებულოა'
   },
   city: (value) => {
-    if (value) {
-      return true
+    if (!value) {
+      return 'ქალაქი სავალდებულოა'
     }
-    return 'you must choose city'
+    return true
   },
   price: (value) => {
     if (value) {
-      return true
+      return 'ფასი სავალდებულოა'
+    } else if (isNaN(value) || value.trim() === '') {
+      return 'უნდა შედგებოდეს რიცხვებისგან'
     }
-    return 'you must choose price'
+    return true
   },
   area: (value) => {
     if (value === undefined || value === null || value.trim() === '') {
-      return 'This field is required'
-    }
-    if (isNaN(value) || value.trim() === '') {
-      return 'Value must be numeric'
+      return 'ფართობი სავალდებულოა'
+    } else if (isNaN(value) || value.trim() === '') {
+      return 'უნდა შედგებოდეს რიცხვებისგან'
     }
     return true
   },
   bedrooms: (value) => {
-    if (!value) return 'This field is required'
+    if (!value) return 'საძინებელი სავალდებულოა'
+    else if (isNaN(value) || value.trim() === '') {
+      return 'უნდა შედგებოდეს რიცხვებისგან'
+    }
     const number = Number(value)
-    return Number.isInteger(number) ? true : 'Value must be a whole number'
+    return Number.isInteger(number) ? true : 'უნდა იყოს მთელი რიცხვი'
   },
   description: (value) => {
     if (value === undefined || value === null || value.trim() === '') {
-      return 'This field is required'
+      return 'აღწერა სავალდებულოა'
     }
     const wordCount = value.trim().split(/\s+/).length
-    return wordCount > 5 ? true : `Must be more than 5 words`
+    return wordCount > 4 ? true : `უნდა იყოს 5 სიტყვაზე მეტი`
   },
   agent: (value) => {
     if (value) {
       return true
     }
-    return 'you must choose an agent'
+    return 'აგენტი სავალდებულოა'
   }
 }
