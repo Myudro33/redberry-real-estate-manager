1d41103fd41e9177d5420910fcf92d0032045c2e
myudro33 <qanashvilinika7@gmail.com>
2024-09-16 21:45:37 +0400

    feat: filter by region

--- src/components/filter/RegionFilter.vue
+++ src/components/filter/RegionFilter.vue
@@ -1,7 +1,19 @@
 <script setup>
+import { ref } from 'vue';
 import TheButton from '../TheButton.vue';
 import { useEstateStore } from '@/stores';
 const estateStore = useEstateStore()
+const emits = defineEmits(['region'])
+const filters = ref([])
+const filter = (e) => {
+    if (filters.value.includes(e.target.value)) {
+        return filters.value.splice(filters.value.indexOf(e.target.value), 1);
+    }
+    return filters.value.push(e.target.value)
+}
+const submit = () => {
+    emits('region', filters.value)
+}
 </script>
 
 <template>
@@ -11,12 +23,12 @@ const estateStore = useEstateStore()
         <div class="w-full flex flex-wrap mt-6 ">
             <div class="w-1/3 h-6 flex items-center font-normal text-sm " v-for="item in estateStore.regions"
                 :key="item.id">
-                <input class="w-5 h-5" type="checkbox" :name="item.name" />
-                <label class="ml-2" for="scales">{{ item.name }}</label>
+                <input @change="filter" :value="item.name" class="w-5 h-5" type="checkbox" :name="item.name" />
+                <label class="ml-2" :for="item.name">{{ item.name }}</label>
             </div>
         </div>
         <div class="w-full flex justify-end mt-auto">
-            <TheButton :background="true" title="არჩევა" />
+            <TheButton @click="submit" :background="true" title="არჩევა" />
         </div>
     </div>
 </template>
