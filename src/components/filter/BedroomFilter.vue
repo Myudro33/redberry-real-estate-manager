c0d8caae3be47cb10386d3bb1ba55e772399b2b5
myudro33 <qanashvilinika7@gmail.com>
2024-09-16 21:45:21 +0400

    feat: filter by bedrooms

--- src/components/filter/BedroomFilter.vue
+++ src/components/filter/BedroomFilter.vue
@@ -1,5 +1,11 @@
 <script setup>
+import { ref } from 'vue';
 import TheButton from '../TheButton.vue';
+const inputValue = ref('')
+const emits = defineEmits('')
+const submit = () => {
+    emits('bedrooms', inputValue.value)
+}
 </script>
 
 <template>
@@ -7,10 +13,11 @@ import TheButton from '../TheButton.vue';
         class="flex flex-col z-50 bg-white w-72 p-6 rounded-lg border border-border-color shadow-box-shadow h-48 absolute top-14 right-[-1rem]">
         <h1 class="font-medium">საძინებლების რაოდენობა</h1>
         <div class="w-full flex flex-wrap mt-6 ">
-            <input class="w-11 h-11 border border-input-border px-4 rounded-md" placeholder="2" type="text">
+            <input v-model="inputValue" class="w-11 h-11 border border-input-border px-4 rounded-md" placeholder="2"
+                type="text">
         </div>
         <div class="w-full flex justify-end mt-auto">
-            <TheButton :background="true" title="არჩევა" />
+            <TheButton @click="submit" :background="true" title="არჩევა" />
         </div>
     </div>
 </template>
