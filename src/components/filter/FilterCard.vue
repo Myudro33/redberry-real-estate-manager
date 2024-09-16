d6a0e7ad05d239688a36b3252645b5241f5fef52
myudro33 <qanashvilinika7@gmail.com>
2024-09-17 00:00:39 +0400

    refactor: create filter card

---
+++ src/components/filter/FilterCard.vue
@@ -0,0 +1,12 @@
+<template>
+    <span class="flex first:ml-0 ml-2 items-center rounded-3xl border border-border-color px-2 py-1">{{
+        props.title }}
+        <DeleteIcon @click="props.function" class="ml-1" />
+    </span>
+</template>
+
+<script setup>
+import DeleteIcon from '../icons/DeleteIcon.vue';
+
+const props = defineProps(['title', 'function'])
+</script>
