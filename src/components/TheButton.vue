5d9b15ceb3ba31c200b79e462bac3977fd9edb32
myudro33 <qanashvilinika7@gmail.com>
2024-09-18 23:28:25 +0400

    chore: change function name

--- src/components/TheButton.vue
+++ src/components/TheButton.vue
@@ -20,13 +20,13 @@ const click = () => {
         router.push({ query: null })
     }
 }
-const test = () => {
+const clearLocalStorage = () => {
     localStorage.clear()
 }
 </script>
 
 <template>
-    <router-link @click="test" :to="{ name: props.routerTo }" v-if="props.type === 'link'"
+    <router-link @click="clearLocalStorage" :to="{ name: props.routerTo }" v-if="props.type === 'link'"
         :class="[props.background ? 'text-white bg-button-background hover:bg-button-background-hover' : 'bg-transparent hover:bg-button-background border border-button-background text-button-background hover:text-white']"
         class="px-4 py-[10px] h-12 text-base font-medium rounded-[10px]">{{ props.title
         }}</router-link>
