a5effa776d93031b9247cc7f497a6ea5f2b73055
myudro33 <qanashvilinika7@gmail.com>
2024-09-21 19:12:48 +0400

    style: choose region with label title

--- src/components/filter/RegionFilter.vue
+++ src/components/filter/RegionFilter.vue
@@ -26,10 +26,10 @@ const submit = () => {
         class="flex z-50 flex-col bg-white w-[45.6rem] p-6 rounded-lg border border-border-color shadow-box-shadow h-72 absolute top-14">
         <h1 class="font-medium">რეგიონის მიხედვით</h1>
         <div class="w-full flex flex-wrap mt-6 ">
-            <div class="w-1/3 h-6 flex items-center font-normal text-sm " v-for="item in estateStore.regions"
+            <div class="w-1/3 h-8 flex items-center font-normal text-sm" v-for="item in estateStore.regions"
                 :key="item.id">
                 <input :checked="checked?.includes(item.name)" @change="filter" :value="item.name" class="w-5 h-5"
-                    type="checkbox" :name="item.name" />
+                    type="checkbox" :id="item.name" :name="item.name" />
                 <label class="ml-2" :for="item.name">{{ item.name }}</label>
             </div>
         </div>
