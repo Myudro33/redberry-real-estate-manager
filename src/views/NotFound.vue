1c8b7327c742eb32290524354faff7fff6ddd289
myudro33 <qanashvilinika7@gmail.com>
2024-09-21 23:01:39 +0400

    feat: add wrong url page

---
+++ src/views/NotFound.vue
@@ -0,0 +1,11 @@
+<template>
+    <div class="w-screen h-screen flex flex-col items-center justify-center">
+        <NotFoundIcon class="w-96 h-96" />
+        <h1 class="text-4xl font-bold font-firago">გვერდი არ მოიძებნა</h1>
+    </div>
+</template>
+
+
+<script setup>
+import NotFoundIcon from '@/components/icons/NotFoundIcon.vue';
+</script>
