e8645d0ac336ddeb33c390ddd22f9e1df1d9ebee
myudro33 <qanashvilinika7@gmail.com>
2024-09-21 23:04:31 +0400

    feat: add router link to return home from wrong url

--- src/views/NotFound.vue
+++ src/views/NotFound.vue
@@ -1,11 +1,13 @@
 <template>
     <div class="w-screen h-screen flex flex-col items-center justify-center">
         <NotFoundIcon class="w-96 h-96" />
-        <h1 class="text-4xl font-bold font-firago">გვერდი არ მოიძებნა</h1>
+        <h1 class="text-4xl font-bold font-firago">გვერდი არ მოიძებნა :(</h1>
+        <TheButton class="mt-6" title="საწყისზე დაბრუნება" router-to="home" :background="true" type="link" />
     </div>
 </template>
 
 
 <script setup>
 import NotFoundIcon from '@/components/icons/NotFoundIcon.vue';
+import TheButton from '@/components/TheButton.vue';
 </script>
